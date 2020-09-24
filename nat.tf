d8fd8d178997c952ae2d5405fe9e4175dacc1ebf
C S <pandrajucs@example.com>
2020-09-24 15:02:15 +0530

    first commit

---
+++ nat.tf
@@ -0,0 +1,30 @@
+//craete NGW
+
+ resource "aws_nat_gateway" "natgw" {
+    
+  allocation_id = "${aws_eip.lb.id}"
+  subnet_id     = "subnet-034250c45e4a08fbf"
+
+  tags = {
+    Name = "NAT-GW"
+  }
+}
+//create RT for NAT GW
+
+resource "aws_route_table" "NAT-RT" {
+
+  vpc_id = "vpc-03fce764d6d3fdd1f"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+   tags = {
+    Name = "NAT-RT"
+  }
+}
+
+//create route table association
+resource "aws_route_table_association" "RouteTableAss" {
+  
+  route_table_id = "${aws_route_table.NAT-RT.id}"
+}
---
+++ servers.tf
@@ -0,0 +1,71 @@
+//create Private Server
+
+ resource "aws_instance" "server-1" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-0febc75bdca1a539b"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script1.sh")}"
+    tags = {
+        Name = "Private-Server-1"
+    }
+} 
+
+resource "aws_instance" "server-2" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-09f3748a93e1261ae"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script2.sh")}"
+    tags = {
+        Name = "Private-Server-2"
+    }
+} 
+
+resource "aws_instance" "server-3" {
+    ami = "ami-00514a528eadbc95b"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "AWS_KeyPair"
+    subnet_id = "subnet-0fe01486fd69660bc"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    user_data = "${file("script3.sh")}"
+    tags = {
+        Name = "Private-Server-3"
+    }
+} 
+
+resource "aws_lb_target_group_attachment" "tg1" {
+  target_group_arn = "${aws_lb_target_group.tg.arn}"
+  target_id        = "${aws_instance.server-1.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg1" {
+  target_group_arn = "${aws_lb_target_group.tg.arn}"
+  target_id        = "${aws_instance.server-2.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg1" {
+  target_group_arn = "${aws_lb_target_group.tg.arn}"
+  target_id        = "${aws_instance.server-3.id}"
+  port             = 80
+}
+
+ resource "aws_lb_listener" "web" {
+  load_balancer_arn = "${aws_lb.nlb.arn}"
+  port              = "80"
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.tg.arn}"
+  }
+}   
